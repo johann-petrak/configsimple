f644a47e8985c34dcd409b9fbbb97f5b8f479f06
Johann Petrak <johann.petrak@gmail.com>
2019-06-03 12:52:45 +0200

    Continue with basic layout

--- setup.py
+++ setup.py
@@ -7,7 +7,8 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-readme = open(os.path.join(here, 'README.md')).read()
+with open(os.path.join(here, 'README.md')) as f:
+    readme = f.read()
 
 setup(
     name="simpleconfig",
@@ -16,22 +17,27 @@ setup(
     author_email="johann.petrak@gmail.com",
     description='Configure components/classes using config files, command line options etc in a simple way',
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type='text/markdown',
+    # package_dir = {'': 'simpleconfig'},
     setup_requires=[],
     install_requires=[],
     python_requires=">=3.5",
+    tests_require=[],
+    platforms='any',
     license="MIT",
     keywords="",
     url="http://github.com/johann-petrak/simpleconfig",
-    py_modules=['simpleconfig'],
-    packages=find_packages(),
+    # py_modules=['simpleconfig'],
+    # packages=find_packages(),
+    packages=['simpleconfig'],
     # test_suite='tests',
     # tests_require=['mock'],
-    classifiers=["Development Status :: 5 - Production/Stable",
-                 "License :: OSI Approved :: MIT License",
-                 "Natural Language :: English",
-                 "Programming Language :: Python :: 3",
-                 "Topic :: Software Development",
-                 "Intended Audience :: Developers",
-                ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development",
+        "Intended Audience :: Developers",
+      ],
 )
---
+++ tests/test_simpleconfig1.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestSimpleConfig1C1(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_basics1(self):
+        self.assertEqual(1,1)
