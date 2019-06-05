93eb1abe2fd0155ab6f773ee60ccb8f61edab0c6
Johann Petrak <johann.petrak@gmail.com>
2019-06-05 12:16:09 +0100

    More

--- tests/test_simpleconfig1.py
+++ tests/test_simpleconfig1.py
@@ -6,11 +6,15 @@ from loguru import logger
 logger.remove()
 logger.add(sys.stderr, level="DEBUG")
 
+
+def msg(*args):
+    print(*args, file=sys.stderr)
+
 class TestSimpleConfig1C1():
     #def setUp(self):
     #    pass
 
-    def test_basics1(self):
+    def test_basics1(self, capfd):
         theargs = ["--opt1", "val1", '--comp1.opt2', 'val2']
         topconfig = simpleconfig.SimpleConfig(component='')
         topconfig.add_argument("--opt1", type=str, default="optdefault")
@@ -23,10 +27,12 @@ class TestSimpleConfig1C1():
         assert valopt1 == 'val1'
         valopt2 = comp1config.get('opt2')
         assert valopt2 == 'val2'
-        logger.debug("comp1config ns={}".format(comp1config.namespace))
-        logger.debug("topconfig ns={}".format(topconfig.namespace))
+        with capfd.disabled():
+            logger.debug("comp1config ns={}".format(comp1config.namespace))
+            logger.debug("topconfig ns={}".format(topconfig.namespace))
+            msg("DEBUG MESSAGE")
         valopt2a = topconfig.get('comp1.opt2')
-        assert valopt2a == 'val2'
+        assert valopt2a == 'val22'
 
     def notatest_basics1a(self):
         theargs = ['--foo', "333", "--comp1.help"]
