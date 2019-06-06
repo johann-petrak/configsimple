7052ed31188c54a061249e7cc2647df98bb73633
Johann Petrak <johann.petrak@gmail.com>
2019-06-06 14:00:35 +0100

    Undo debug changes.

--- examples/example1.py
+++ examples/example1.py
@@ -1,5 +1,6 @@
 from simpleconfig import config, SimpleConfig
 
+
 class Component1:
     def __init__(self):
         myconf = SimpleConfig(component="comp1")
@@ -7,6 +8,7 @@ class Component1:
         myconf.add_argument("--foo", default="22", type=int, help="The FOO setting!")
         myconf.parse_args()
         print("Component1 foo is {}".format(myconf.get("foo")))
+
 
 class Component2:
     def __init__(self):
@@ -16,14 +18,14 @@ class Component2:
         myconf.parse_args()
         print("Component2 foo is {}".format(myconf.get("foo")))
 
+
 if __name__ == "__main__":
     config.add_argument("--bar", help="The BAR setting")
     config.add_argument("--foo", help="The toplevel FOO setting")
-    config.add_argument("--com")
-    config.add_argument("--compo", type=int, choices=[1, 2], required=True,  help="Component number")
+    config.add_argument("--comp", type=int, choices=[1, 2], required=True,  help="Component number")
     config.parse_args()
     print("Toplevel foo is {}".format(config.get("foo")))
-    compclass = [Component1, Component2][config.get("compo")-1]
+    compclass = [Component1, Component2][config.get("comp")-1]
     comp = compclass()
     print("Get the global comp1.foo: {}".format(config.get("comp1.foo")))
     print("Get the global comp2.foo: {}".format(config.get("comp2.foo")))
